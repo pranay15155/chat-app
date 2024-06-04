96808fd57cb20745eff18b370e441dafe9e24eb3
pranay15155 <pranayprasoon15@gmail.com>
2024-06-04 11:08:06 +0530

    update2

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import { app, server } from "./socket/socket.js";
 
 //file imports
 import authRoutes from "./routes/auth.routes.js";
@@ -13,7 +13,7 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 //variables
-const app=express();
+// const app=express();
 const PORT = process.env.PORT || 5000;
 
 
@@ -37,7 +37,7 @@ app.use("/api/users", userRoutes);
 
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`server running on port ${PORT}`);
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 
-// import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-// import SignUp from "./pages/signup/SignUp";
-// import { Toaster } from "react-hot-toast";
-// import { useAuthContext } from "./context/AuthContext";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-
+const { authUser } = useAuthContext();
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        {/* <Routes>
+        <Routes>
           <Route
             path="/"
             element={authUser ? <Home /> : <Navigate to={"/login"} />}
@@ -25,11 +25,8 @@ function App() {
             path="/signup"
             element={authUser ? <Navigate to="/" /> : <SignUp />}
           />
-        </Routes> */}
-        {/* <Toaster /> */}
-
-        {/* <SignUp/> */}
-        <Home></Home>
+        </Routes>
+        <Toaster />
       </div>
     </>
   );
