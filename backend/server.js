70d76a554e1142e760163e3a30b0a04b6316dcda
pranay15155 <pranayprasoon15@gmail.com>
2024-06-04 11:32:57 +0530

    update3

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //package imports
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -19,6 +20,9 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+
+const __dirname = path.resolve();
+
 //MIDDLEWARES
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -35,7 +39,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,()=>{
     connectToMongoDB();
