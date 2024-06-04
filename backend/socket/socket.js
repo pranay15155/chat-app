adf5d527b5fdb98fb27fa087f9bea594706c314d
pranay15155 <pranayprasoon15@gmail.com>
2024-06-04 21:00:54 +0530

    update5

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://chat-app-1-un6k.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
